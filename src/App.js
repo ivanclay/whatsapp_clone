817da11d21cb93906ed006f500a54fb329371357
ivanclay <ivan.clay.moura@gmail.com>
2021-09-30 18:05:24 -0300

    Create project
    Create README
    Install dependencies (firebase, material-ui/core, material-ui/icons, emoji-picker-react)
    Configure app area

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './App.css';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+  return (
+    <div className="app-window">
+      <div className="sidebar">...SIDE</div>
+      <div className="contentarea">...CONTENT</div>
+    </div>
+  );
+}
